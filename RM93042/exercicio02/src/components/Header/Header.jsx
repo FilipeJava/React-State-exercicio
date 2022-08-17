09fcb5b64c4d01d0c255c522d849084ff7a3f7ca
FilipeJava <RM93042@fiap.com.br>
2022-08-17 14:20:57 -0300

    Criando componentes

---
+++ RM93042/exercicio02/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Header(){
+    return(
+
+        <div>
+          <header>
+            <h1>Titulo</h1>
+            <p>paragrafo</p>
+          </header>
+        </div>
+
+    )
+}
